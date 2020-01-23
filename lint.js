82aeffde8b5f517fe8a78e6aac887fee06b8f53f
Francois Daoust <fd@tidoust.net>
2020-01-23 19:20:31 +0100

    Add JSON Schema and schema validation to linting logic
    Also added a method to slightly improve error messages returned by the JSON
    Schema validation to make them more human-friendly.

--- lint.js
+++ lint.js
@@ -1,6 +1,58 @@
 "use strict";
 
 const fs = require("fs").promises;
+const schema = require("./schema.json");
+const Ajv = require("ajv");
+const ajv = new Ajv();
+
+// When an entry is invalid, the schema validator returns one error for each
+// "oneOf" option and one error on overall "oneOf" problem. This is confusing
+// for humans. the following function improves the error being returned.
+const clarifyErrors = errors => {
+  if (!errors || (errors.length < 2)) {
+    return errors;
+  }
+
+  // If first two errors are type errors for oneOf choices, item is neither
+  // a string nor an object
+  if ((errors[0].schemaPath === "#/items/oneOf/0/type") &&
+      (errors[1].schemaPath === "#/items/oneOf/1/type")) {
+    return [
+      Object.assign(errors[0], { "message": "should be a string or an object" })
+    ];
+  }
+
+  // Otherwise, if second error is a type error for second oneOf choice,
+  // it means the itme is actually a string that represents an invalid URL,
+  // which the first error should capture
+  if (errors[1].schemaPath === "#/items/oneOf/1/type") {
+    return [errors[0]];
+  }
+
+  // Otherwise, item is an object that does not follow the schema, drop the
+  // error that says that item is not a string and the error that says that it
+  // does not meet one of the "oneOf" options. What remains should be the error
+  // that explains why the item does not meet the schema for the object.
+  const clearerErrors = errors.filter(error =>
+      (error.schemaPath !== "#/items/oneOf/0/type") &&
+      (error.schemaPath !== "#/items/oneOf"));
+
+  // Improve an additional property message to point out the property that
+  // should not be there (default message does not say it)
+  clearerErrors.forEach(error => {
+    if ((error.keyword === "additionalProperties") &&
+        error.params && error.params.additionalProperty) {
+      error.message = "should not have additional property '" +
+        error.params.additionalProperty + "'";
+    }
+  });
+
+  // If there are no more errors left to return, roll back to the initial set
+  // to make sure an error gets reported. That should never happen, but better
+  // be ready for it.
+  return (clearerErrors.length > 0) ? clearerErrors : errors;
+};
+
 
 // Lint by normalizing specs.json and comparing it to the original,
 // fixing it in place if |fix| is true.
@@ -8,10 +60,26 @@ async function lint(fix = false) {
   const specsBuffer = await fs.readFile("./specs.json");
   const specs = JSON.parse(specsBuffer);
 
-  const sorted = specs
-    .map(spec => (typeof spec === "string") ? { url: spec } : spec)
-    .map(spec => Object.assign({}, spec, { url: new URL(spec.url).toString() }));
-  sorted.sort((a, b) => a.url.localeCompare(b.url));
+  const isSchemaValid = ajv.validateSchema(schema);
+  if (!isSchemaValid) {
+    throw "The schema.json file must be a valid JSON Schema file";
+  }
+
+  var isValid = ajv.validate(schema, specs, { format: "full" });
+  if (!isValid) {
+    throw ajv.errorsText(clarifyErrors(ajv.errors), {
+      dataVar: "specs", separator: "\n"
+    });
+  }
+
+  const sorted = specs.map(spec => (typeof spec === "string") ?
+      new URL(spec).toString() :
+      Object.assign({}, spec, { url: new URL(spec.url).toString() }));
+  sorted.sort((a, b) => {
+    const aurl = (typeof a === "string") ? a : a.url;
+    const burl = (typeof b === "string") ? b : b.url;
+    return aurl.localeCompare(burl);
+  });
 
   // Prefer URL-only format when we only have a URL
   const fixed = sorted

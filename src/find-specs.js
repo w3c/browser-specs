ad960ea1de8a48831c01e2c3b037ac9fc0482dd8
Dominique Hazael-Massieux <dom@w3.org>
2020-06-10 11:48:31 +0200

    Add comparison with data extracted from TR
    probably helps with #62

--- src/find-specs.js
+++ src/find-specs.js
@@ -7,55 +7,67 @@ const watchedBrowserCgs = ["Web Platform Incubator Community Group", "Web Assemb
 
 const canonicalizeGhUrl = url => (url.indexOf("github.io") > 0 && url.split("/").length === 4 ? url + '/' : url).replace('http:', 'https:');
 
-fetch("https://w3c.github.io/validate-repos/report.json")
-  .then(r => r.json())
-  .then(({groups, repos}) => {
-    const wgs = Object.values(groups).filter(g => g.type === "working group" && !nonBrowserSpecWgs.includes(g.name));
-    const cgs = Object.values(groups).filter(g => g.type === "community group" && watchedBrowserCgs.includes(g.name));
+Promise.all(
+  ["https://w3c.github.io/validate-repos/report.json",
+   "https://w3c.github.io/spec-dashboard/repo-map.json"].map(
+     dataUrl => fetch(dataUrl)
+       .then(r => r.json())
+   )
+).then(([{groups, repos}, specRepos]) => {
+  const wgs = Object.values(groups).filter(g => g.type === "working group" && !nonBrowserSpecWgs.includes(g.name));
+  const cgs = Object.values(groups).filter(g => g.type === "community group" && watchedBrowserCgs.includes(g.name));
 
-    // WGs
-    // * check repos with w3c.json/repo-type includes rec-track
-    const wgRepos = [].concat(...wgs.map(g => g.repos.map(r => r.fullName)))
-          .map(fullName => repos.find(r => fullName === r.owner.login + '/' + r.name));
-    const recTrackRepos = wgRepos.filter(r => r.w3c && r.w3c["repo-type"] && (r.w3c["repo-type"] === 'rec-track' || r.w3c["repo-type"].includes('rec-track')));
-    // * look if those with homepage URLs have a match in the list of specs
-    console.log("URLs from a repo of a browser-spec producing WG with no matching URL in spec list")
-    console.log(recTrackRepos.filter(r => r.homepageUrl)
-                .map(r => canonicalizeGhUrl(r.homepageUrl))
-                .filter(u => !specs.find(s => s.nightly.url === u || (s.release && s.release.url === u)))
-               );
-    // * look if those without a homepage URL have a match with their generated URL
-    Promise.all(recTrackRepos.filter(r => !r.homepageUrl)
-                .map(r => `https://${r.owner.login}.github.io/${r.name}/`)
-                .filter(u => !specs.find(s => s.nightly.url.startsWith(u)))
-                .map(u => fetch(u).then(({status, url}) => {
-                  if (status !== 404) return url;
-                }))).then(urls => {
-                  console.log("Unadvertized URLs from a repo of a browser-spec producing WG with no matching URL in spec list")
-                  console.log(urls.filter(x => x));
-                }
-                         );
+  // WGs
+  // * check repos with w3c.json/repo-type includes rec-track
+  const wgRepos = [].concat(...wgs.map(g => g.repos.map(r => r.fullName)))
+        .map(fullName => repos.find(r => fullName === r.owner.login + '/' + r.name));
+  const recTrackRepos = wgRepos.filter(r => r.w3c && r.w3c["repo-type"] && (r.w3c["repo-type"] === 'rec-track' || r.w3c["repo-type"].includes('rec-track')));
+  // * look if those with homepage URLs have a match in the list of specs
+  console.log("URLs from a repo of a browser-spec producing WG with no matching URL in spec list")
+  console.log(recTrackRepos.filter(r => r.homepageUrl)
+              .map(r => canonicalizeGhUrl(r.homepageUrl))
+              .filter(u => !specs.find(s => s.nightly.url === u || (s.release && s.release.url === u)))
+             );
+  // * look if those without a homepage URL have a match with their generated URL
+  Promise.all(recTrackRepos.filter(r => !r.homepageUrl)
+              .map(r => `https://${r.owner.login}.github.io/${r.name}/`)
+              .filter(u => !specs.find(s => s.nightly.url.startsWith(u)))
+              .map(u => fetch(u).then(({status, url}) => {
+                if (status !== 404) return url;
+              }))).then(urls => {
+                console.log("Unadvertized URLs from a repo of a browser-spec producing WG with no matching URL in spec list")
+                console.log(urls.filter(x => x));
+              }
+                       );
 
-    // CGs
-    //check repos with w3c.json/repo-type includes cg-report or with no w3c.json
-    const cgRepos = [].concat(...cgs.map(g => g.repos.map(r => r.fullName)))
-          .map(fullName => repos.find(r => fullName === r.owner.login + '/' + r.name));
-    const cgSpecRepos = cgRepos.filter(r => !r.w3c || (r.w3c && r.w3c["repo-type"] && (r.w3c["repo-type"] === 'cg-report' || r.w3c["repo-type"].includes('cg-report'))));
-    // * look if those with homepage URLs have a match in the list of specs
-    console.log("URLs from a repo of a browser-spec producing CG with no matching URL in spec list")
-    console.log(cgSpecRepos.filter(r => r.homepageUrl)
-                .map(r => canonicalizeGhUrl(r.homepageUrl))
-                .filter(u => !specs.find(s => s.nightly.url === u))
-               );
-    // * look if those without a homepage URL have a match with their generated URL
-    Promise.all(cgSpecRepos.filter(r => !r.homepageUrl)
-                .map(r => `https://${r.owner.login}.github.io/${r.name}/`)
-                .filter(u => !specs.find(s => s.nightly.url.startsWith(u)))
-                .map(u => fetch(u).then(({status, url}) => {
-                  if (status !== 404) return url;
-                }))).then(urls => {
-                  console.log("Unadvertized URLs from a repo of a browser-spec producing CG with no matching URL in spec list")
-                  console.log(urls.filter(x => x));
-                }
-                         );
-  });
+  // Look which of the specRepos on recTrack from a browser-producing WG have no match
+  console.log("TR specs from browser-producing WGs")
+  console.log(
+    [].concat(...Object.keys(specRepos).map(
+      r => specRepos[r].filter(s => s.recTrack && wgs.find(g => g.id === s.group)).map(s => s.url)))
+      .filter(u  => !specs.find(s => s.release && s.release.url === u))
+  );
+
+  // CGs
+  //check repos with w3c.json/repo-type includes cg-report or with no w3c.json
+  const cgRepos = [].concat(...cgs.map(g => g.repos.map(r => r.fullName)))
+        .map(fullName => repos.find(r => fullName === r.owner.login + '/' + r.name));
+  const cgSpecRepos = cgRepos.filter(r => !r.w3c || (r.w3c && r.w3c["repo-type"] && (r.w3c["repo-type"] === 'cg-report' || r.w3c["repo-type"].includes('cg-report'))));
+  // * look if those with homepage URLs have a match in the list of specs
+  console.log("URLs from a repo of a browser-spec producing CG with no matching URL in spec list")
+  console.log(cgSpecRepos.filter(r => r.homepageUrl)
+              .map(r => canonicalizeGhUrl(r.homepageUrl))
+              .filter(u => !specs.find(s => s.nightly.url === u))
+             );
+  // * look if those without a homepage URL have a match with their generated URL
+  Promise.all(cgSpecRepos.filter(r => !r.homepageUrl)
+              .map(r => `https://${r.owner.login}.github.io/${r.name}/`)
+              .filter(u => !specs.find(s => s.nightly.url.startsWith(u)))
+              .map(u => fetch(u).then(({status, url}) => {
+                if (status !== 404) return url;
+              }))).then(urls => {
+                console.log("Unadvertized URLs from a repo of a browser-spec producing CG with no matching URL in spec list")
+                console.log(urls.filter(x => x));
+              }
+                       );
+});

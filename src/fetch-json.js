ed74375909a654a30423c5820c22f6a7989e96a2
Francois Daoust <fd@tidoust.net>
2025-04-30 08:56:31 +0200

    Throttle JSON fetches per origin
    The `fetchJSON` function was only applying loose throttling, but
    https://raw.githubusercontent.com is starting to return 429 responses after a
    while (we send a few hundreds of requests to retrieve `w3c.json` files).
    
    Documentation does not say much, but it would seem the limit is around ~5000
    requests per hour. This update makes the function sleep 1000ms in between
    requests to a given origin.
    
    This will likely add ~10mn of (idle) build time.

--- src/fetch-json.js
+++ src/fetch-json.js
@@ -1,7 +1,13 @@
 import ThrottledQueue from "./throttled-queue.js";
 
 // Make sure we remain "friendly" with servers
-const fetchQueue = new ThrottledQueue({ maxParallel: 2 });
+// In particular, we're going to have to fetch a number of w3c.json files from
+// https://raw.githubusercontent.com which seems to restrict the total number
+// of allowed requests to ~5000 per hour and per IP address.
+const fetchQueue = new ThrottledQueue({
+  maxParallel: 4,
+  sleepInterval: 1000
+});
 
 // Maintain a cache of fetched JSON resources in memory to avoid sending the
 // same fetch request again and again
@@ -14,7 +20,7 @@ export default async function (url, options) {
   if (cache[url]) {
     return structuredClone(cache[url]);
   }
-  const res = await fetchQueue.runThrottled(fetch, url, options);
+  const res = await fetchQueue.runThrottledPerOrigin(url, fetch, url, options);
   if (res.status === 404) {
     return null;
   }
